1ea09068302287cc4a44e5f02d8ae24189b9edee
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2020-05-27 21:55:38 +0800

    Updates

--- sortingAlgo/bubble.js
+++ sortingAlgo/bubble.js
@@ -2,17 +2,17 @@ import {swap} from "../script.js"
 
 
 export default function bubble(arr){
-let array = arr,
+let array = arr.slice(),
 len = array.length,
 animation = []
 
     for (let i = len-1; i>=0; i--){
-    for(let j = 1; j<=i; j++){
-        if(array[j-1] > array[j]){
-        swap(array, j-1, j)
-        } 
+        for(let j = 1; j<=i; j++){
+            if(array[j-1] > array[j]){
+            animation.push(array.slice(0))
+            swap(array, j-1, j)
+            } 
+        }
     }
-    animation.push(array.slice(0))
-    }
-    return animation
+return animation
 }
